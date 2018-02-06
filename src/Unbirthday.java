3fc7da9edc2fc001288366418353158031f17d05
yuandrew2014 <league@WorthyMini-6.attlocal.net>
2018-02-05 16:57:59 -0800

    Unbirthday IP

---
+++ src/Unbirthday.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	
+	String name = JOptionPane.showInputDialog("When is your birthday");
+	if (name.equals("02/05"){
+		JOptionPane.showMessageDialog(null, "Happy Birthday");
+}
+JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+	
+}	
+}
+
