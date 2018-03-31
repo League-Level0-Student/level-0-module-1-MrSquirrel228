863f88c21a292aef0814b47166aa725ec91a2276
MrSquirrel228 <league@iMac.attlocal.net>
2018-03-31 11:33:23 -0700

    Complete Badger/ Rotten tomato wip

--- src/Badger.java
+++ src/Badger.java
@@ -4,21 +4,22 @@ import javax.swing.JOptionPane;
 
 public class Badger {
 	public static void main(String[] args) {
+		for (int E = 1; E < 4; E++) {
+			for (int B = 1; B < 11; B++) {
+				JOptionPane.showMessageDialog(null, "Badger");
 
-		for (int A = 1; A < 3; A++) {
-			for (int E = 1; E < 4; E++) {
-				for (int B = 1; B < 11; B++) {
-					JOptionPane.showMessageDialog(null, "Badger");
-				}
+			}
+			if (E < 3) {
+
 				for (int C = 1; C < 3; C++) {
 					JOptionPane.showMessageDialog(null, "mushroom");
 				}
-			}
-			for (int D = 1; D < 4; D++) {
-				JOptionPane.showMessageDialog(null, "a snake");
-			}
+			} else {
 
+				for (int D = 1; D < 4; D++) {
+					JOptionPane.showMessageDialog(null, "a snake");
+				}
+			}
 		}
 	}
 }
-
---
+++ src/Rotton_Tomato/Rotton_Tomato.pde
@@ -0,0 +1,18 @@
+int x = mouseX;
+int y = mouseY;
+void setup() {
+size(500, 500);
+}
+void draw() {   
+  background(200, 200, 200);
+    noStroke();
+    fill (255, 84, 84);
+    ellipse(150, 200, 150, 150);
+    ellipse(212, 200, 150, 150);
+    fill (26, 134, 30);
+    rect(176, 103, 12, 32);
+    if (mousePressed){
+    fill(255, 255, 255);
+    ellipse (mouseX, mouseY, 60, 60);
+}}
+        
